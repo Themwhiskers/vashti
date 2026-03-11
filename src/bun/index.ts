0499b6062e280bc277fdd69c3f5836788a70e25a
Themwhiskers <zvika.sage@gmail.com>
2026-03-11 21:46:04 +0200

    applet does not load

--- src/bun/index.ts
+++ src/bun/index.ts
@@ -1,8 +1,16 @@
-import { BrowserWindow } from "electrobun/bun";
+import { BrowserWindow, createRPC } from "electrobun/bun";
 
 const isDev = process.env.NODE_ENV === "development";
 
 const win = new BrowserWindow({
   title: "Vashti",
   url: isDev ? "http://localhost:5173" : "views://bar-ui/index.html",
+  rpc: createRPC({
+    openMyApplet: () => {
+      new BrowserWindow({
+        title:"Keybinds Helper",
+        url: "views://keybinds-helper/index.html",
+      })
+    }
+  }),
 });
