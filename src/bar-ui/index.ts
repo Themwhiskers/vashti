4eabe9a1a23756783329241923cd9fa324d58365
Themwhiskers <zvika.sage@gmail.com>
2026-03-11 22:34:38 +0200

    yeash..

--- src/bar-ui/index.ts
+++ src/bar-ui/index.ts
@@ -1,12 +1,10 @@
-import { Electroview } from "electrobun/view";
+import { Electroview, createRPC } from "electrobun/view";
 
-// Instantiate the electrobun browser api
-const electrobun = new Electroview({ rpc: null });
+const electrobun = new Electroview({ rpc: createRPC({}) });
 
 window.loadPage = () => {
   const newUrl = document.querySelector("#urlInput").value;
   const webview = document.querySelector(".webview");
-
   webview.src = newUrl;
 };
 
@@ -19,3 +17,8 @@ window.goForward = () => {
   const webview = document.querySelector(".webview");
   webview.goForward();
 };
+
+// This is what was missing:
+document.querySelector("#myBtn").addEventListener("click", () => {
+  electrobun.rpc.openMyApplet();
+});
